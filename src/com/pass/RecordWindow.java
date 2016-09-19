32bcc9c25d92c2e58aa13aef25a4c52087b6642c
soham427 <sohamsarkar427@gmail.com>
2016-09-19 19:18:51 +1200

    Update RecordWindow.java

--- src/com/pass/RecordWindow.java
+++ src/com/pass/RecordWindow.java
@@ -1,5 +1,4 @@
 package com.pass;
-
 import java.awt.Dimension;
 
 import javax.swing.Box;
@@ -7,48 +6,38 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import java.util.*;
 
 public class RecordWindow extends JFrame implements Observer {
-    private final int COMP_GAP = 30;
-
     private RecordTable recordTable;
-    private JLabel lblSelectedCell;
-    private Observable parentWindow;
+	JLabel lblOutput;
+    private final int COMP_GAP = 30;
+	LoginWindow ui;
 
-
-    public RecordWindow(int pos_x, int pos_y, int width, int height, String title, Observable parentWindow) {
+    public RecordWindow(int pos_x, int pos_y, int width, int height, String title, LoginWindow ui) {
         setTitle(title);
         setBounds(pos_x, pos_y, width, height);
         setMinimumSize(new Dimension(width, height));
-
-        this.parentWindow = parentWindow;
-        this.parentWindow.notifyAllObservers(); // Notify everyone
+		
+		this.ui=ui;
+		this.ui.newWindowOpened();
+		
 
         addComponents();
-        addListeners();
         loadRecordData();
         setVisible(true);
     }
 
     // Add JComponents to the window
     private void addComponents() {
-        recordTable = new RecordTable();
-        lblSelectedCell = new JLabel("Selected Text: ");
+        recordTable = new RecordTable(this);
+		lblOutput = new JLabel();
 
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS)); // JFrame's layout
         add(Box.createRigidArea(new Dimension(new Dimension(COMP_GAP, COMP_GAP)))); // Gap
         add(new JScrollPane(recordTable));
         add(Box.createRigidArea(new Dimension(new Dimension(COMP_GAP, COMP_GAP)))); // Gap
-        add(new JScrollPane(lblSelectedCell));
-        add(Box.createRigidArea(new Dimension(new Dimension(COMP_GAP, COMP_GAP)))); // Gap
-    }
-
-    private void addListeners() {
-        // I won't spoil the challenge task, so here it only implements the SelectionListener
-        recordTable.getSelectionModel().addListSelectionListener(e -> {
-            lblSelectedCell.setText("Selected Text: "
-                    + recordTable.getModel().getValueAt(recordTable.getSelectedRow(), recordTable.getSelectedColumn()));
-        });
+		add(lblOutput);
     }
 
     // Load user-record from the disk
@@ -71,12 +60,10 @@ public class RecordWindow extends JFrame implements Observer {
         recordTable.addRow(new Object[] {"God", userRecord.godCount});
 
     }
-
-    @Override
-    public void update() {
-        // Basically reload the data and update rows
-        recordTable.resetRowCounter();
+	
+	@Override
+	public void update(Observable o, Object ob){
+		recordTable.resetRowCounter();
         loadRecordData();
-        addListeners();
-    }
+	}
 }
