97333f8a878a845f518e1de1455c9168900e448d
soham427 <sohamsarkar427@gmail.com>
2016-09-19 19:18:02 +1200

    Update RecordTable.java

--- src/com/pass/RecordTable.java
+++ src/com/pass/RecordTable.java
@@ -1,14 +1,18 @@
 package com.pass;
-
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.event.*;
+import java.text.SimpleDateFormat;
+import java.io.*;
 
-public class RecordTable extends JTable {
+public class RecordTable extends JTable implements ListSelectionListener {
     private static final String[] COLUMN_NAMES = new String[]{"User Type", "No. of Attempts"};
     private int lastAddedRow;
-
-    public RecordTable() {
+	private RecordWindow rw;
+    public RecordTable(RecordWindow rw) {
+		this.rw=rw;
         this.setModel(new DefaultTableModel(COLUMN_NAMES, 4));
+		this.getSelectionModel().addListSelectionListener(this);
     }
 
     public void addRow(Object[] rowData) {
@@ -17,9 +21,16 @@ public class RecordTable extends JTable {
         }
         lastAddedRow++;
     }
-
-    // Challenge: Refactor to avoid repeating code
-    public void resetRowCounter() {
-        lastAddedRow = 0;
-    }
+	
+	public void resetRowCounter(){
+		lastAddedRow =0;
+	}
+	
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		File f = new File(LoginWindow.FILE_STORAGE_PATH);
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		
+		rw.lblOutput.setText("<html>"+this.getValueAt(this.getSelectedRow(),0).toString()+" has logged in "+this.getValueAt(this.getSelectedRow(),1).toString()+" time(s).<br>Last Modified: "+sdf.format(f.lastModified())+"</html>");
+	}
 }
